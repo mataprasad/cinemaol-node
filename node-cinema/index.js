896bbdbd0056352b017e319d7d8881350836a39a
Mata Prasad Chauhan <mata.chauhan@yatraonline.local>
2017-09-02 17:10:53 +0530

    updated comment

--- node-cinema/index.js
+++ node-cinema/index.js
@@ -28,9 +28,8 @@ app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
     extended: true
 }));
-//app.use(express.methodOverride());
-//app.use(express.cookieParser());
 
+//use before router
 app.use(session({
     store: new SQLiteStore,
     secret: 'keyboard cat',
